d27ef8ffe9bb45347029a51efddea394d68249fe
michaelxxf <miokonkwo023@gmail.com>
2025-01-23 22:15:36 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,232 @@
+
+
+
+        <footer class="footer-area section-gap">
+          <div class="container">
+            <div class="row">
+                          <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                                  <img src="/img/image.png">
+  </div>
+  </div>
+              <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                  <h4>About IMT</h4>
+                  <ul>
+                    <li><a href="#">About IMT</a></li>
+                    <li><a href="#">Leadership</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Contact US</a></li>
+                  </ul>								
+                </div>
+              </div>
+              <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                  <h4>Academics</h4>
+                  <ul>
+                    <li><a href="#">Programmes</a></li>
+                    <li><a href="#">Course Registeration</a></li>
+                    <li><a href="#">Academic Policies</a></li>
+                    <li><a href="#">Academic Calender</a></li>
+                  </ul>								
+                </div>
+              </div>
+              <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                  <h4>Admission</h4>
+                  <ul>
+                    <li><a href="#">Admisssion Process</a></li>
+                    <li><a href="#">Find a Course</a></li>
+                    <li><a href="#">Apply</a></li>
+                    <li><a href="#">Admission Result Checker</a></li>
+                  </ul>								
+                </div>
+              </div>																		
+              <div class="col-lg-4  col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                  <h4>Newsletter</h4>
+                  <p>Stay update with our latest</p>
+                  <div class="" id="mc_embed_signup">
+                    <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+  <div class="input-wrapper">
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <g data-name="Layer 2">
+        <g data-name="inbox">
+          <rect
+            width="24"
+            height="24"
+            transform="rotate(180 12 12)"
+            opacity="0"
+          ></rect>
+          <path
+            d="M20.79 11.34l-3.34-6.68A3 3 0 0 0 14.76 3H9.24a3 3 0 0 0-2.69 1.66l-3.34 6.68a2 2 0 0 0-.21.9V18a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-5.76a2 2 0 0 0-.21-.9zM8.34 5.55a1 1 0 0 1 .9-.55h5.52a1 1 0 0 1 .9.55L18.38 11H16a1 1 0 0 0-1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2a1 1 0 0 0-1-1H5.62z"
+          ></path>
+        </g>
+      </g>
+    </svg>
+    <input type="text" name="text" class="input" placeholder="info@gmail.com" />
+    <button class="Subscribe-btn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="10"
+        viewBox="0 0 38 15"
+        class="arrow"
+      >
+        <path
+          d="M10 7.519l-.939-.344h0l.939.344zm14.386-1.205l-.981-.192.981.192zm1.276 5.509l.537.843.148-.094.107-.139-.792-.611zm4.819-4.304l-.385-.923h0l.385.923zm7.227.707a1 1 0 0 0 0-1.414L31.343.448a1 1 0 0 0-1.414 0 1 1 0 0 0 0 1.414l5.657 5.657-5.657 5.657a1 1 0 0 0 1.414 1.414l6.364-6.364zM1 7.519l.554.833.029-.019.094-.061.361-.23 1.277-.77c1.054-.609 2.397-1.32 3.629-1.787.617-.234 1.17-.392 1.623-.455.477-.066.707-.008.788.034.025.013.031.021.039.034a.56.56 0 0 1 .058.235c.029.327-.047.906-.39 1.842l1.878.689c.383-1.044.571-1.949.505-2.705-.072-.815-.45-1.493-1.16-1.865-.627-.329-1.358-.332-1.993-.244-.659.092-1.367.305-2.056.566-1.381.523-2.833 1.297-3.921 1.925l-1.341.808-.385.245-.104.068-.028.018c-.011.007-.011.007.543.84zm8.061-.344c-.198.54-.328 1.038-.36 1.484-.032.441.024.94.325 1.364.319.45.786.64 1.21.697.403.054.824-.001 1.21-.09.775-.179 1.694-.566 2.633-1.014l3.023-1.554c2.115-1.122 4.107-2.168 5.476-2.524.329-.086.573-.117.742-.115s.195.038.161.014c-.15-.105.085-.139-.076.685l1.963.384c.192-.98.152-2.083-.74-2.707-.405-.283-.868-.37-1.28-.376s-.849.069-1.274.179c-1.65.43-3.888 1.621-5.909 2.693l-2.948 1.517c-.92.439-1.673.743-2.221.87-.276.064-.429.065-.492.057-.043-.006.066.003.155.127.07.099.024.131.038-.063.014-.187.078-.49.243-.94l-1.878-.689zm14.343-1.053c-.361 1.844-.474 3.185-.413 4.161.059.95.294 1.72.811 2.215.567.544 1.242.546 1.664.459a2.34 2.34 0 0 0 .502-.167l.15-.076.049-.028.018-.011c.013-.008.013-.008-.524-.852l-.536-.844.019-.012c-.038.018-.064.027-.084.032-.037.008.053-.013.125.056.021.02-.151-.135-.198-.895-.046-.734.034-1.887.38-3.652l-1.963-.384zm2.257 5.701l.791.611.024-.031.08-.101.311-.377 1.093-1.213c.922-.954 2.005-1.894 2.904-2.27l-.771-1.846c-1.31.547-2.637 1.758-3.572 2.725l-1.184 1.314-.341.414-.093.117-.025.032c-.01.013-.01.013.781.624zm5.204-3.381c.989-.413 1.791-.42 2.697-.307.871.108 2.083.385 3.437.385v-2c-1.197 0-2.041-.226-3.19-.369-1.114-.139-2.297-.146-3.715.447l.771 1.846z"
+        ></path></svg>
+      >Subscribe
+    </button>
+  </div>
+
+                          <div class="info"></div>
+                      </div>
+                    </form> 
+                        <!-- From Uiverse.io by UwUTastisch -->
+                      <a href="wa.me//+2348089326713">
+                        <div class="tooltip-container ">
+                          <span class="tooltip">Message Us</span>
+                          <span class="text">
+                            <div class="borde-back">
+                            <div class="icon">
+                            <div class="icon">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="37"
+                                height="37"
+                                class="bi bi-whatsapp"
+                                viewBox="0 0 16 16"
+                                >
+                                <path
+                                  d="M13.601 2.326a7.537 7.537 0 0 0-10.667 0c-2.944 2.945-2.944 7.717 0 10.662l-.864 3.153a.437.437 0 0 0 .544.544l3.151-.863a7.537 7.537 0 0 0 10.667-10.666Zm-5.88 11.915a6.453 6.453 0 0 1-3.46-.98l-.25-.145-2.349.645.626-2.28-.162-.252a6.465 6.465 0 0 1-.976-3.462A6.48 6.48 0 0 1 13.086 3.87 6.478 6.478 0 0 1 8.72 14.241Zm3.644-4.712c-.198-.1-1.173-.58-1.354-.646-.18-.065-.312-.1-.443.1-.13.2-.508.646-.622.777-.115.13-.23.146-.426.046a5.338 5.338 0 0 1-1.571-1.148 5.953 5.953 0 0 1-1.117-1.405c-.115-.197-.013-.303.087-.402.09-.09.198-.23.296-.345a1.373 1.373 0 0 0 .198-.335c.067-.133.033-.25-.017-.345-.05-.096-.443-1.068-.607-1.464-.16-.387-.323-.334-.443-.34l-.372-.008a.73.73 0 0 0-.533.25A2.23 2.23 0 0 0 4.9 6.99a3.872 3.872 0 0 0 .813 2.376 8.882 8.882 0 0 0 3.536 3.153c.493.213.881.34 1.182.435.497.157.95.134 1.307.08.398-.06 1.173-.48 1.338-.94.165-.46.165-.85.115-.94-.05-.09-.182-.13-.38-.23Z"
+                                ></path>
+                                </svg>
+                              </div>
+
+                                                      </div>
+                                                    </span>
+                                                    </div>
+                    </a>
+                          <!-- From Uiverse.io by UwUTastisch  End-->
+                  </div>
+                </div>
+              </div>											
+            </div>
+            <div class="footer-bottom row align-items-center justify-content-between">
+              <p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+              <!-- From Uiverse.io by wilsondesouza --> 
+  <ul class="example-2">
+    <li class="icon-content">
+      <a
+        data-social="whatsapp"
+        aria-label="Whatsapp"
+        href="https://api.whatsapp.com/send?phone=+2348089326713&amp;text=Save%20this%20to%20your%20Favorites%20-%20@wilsondesouza"
+      >
+        <div class="filled"></div>
+        <svg
+          xml:space="preserve"
+          viewBox="0 0 24 24"
+          class="bi bi-whatsapp"
+          fill="currentColor"
+          height="24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="currentColor"
+            d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"
+          ></path>
+        </svg>
+      </a>
+      <div class="tooltip">Whatsapp</div>
+    </li>
+    <li class="icon-content">
+      <a
+        data-social="facebook"
+        aria-label="Facebook"
+        href="https://www.facebook.com/"
+      >
+        <div class="filled"></div>
+        <svg
+          xml:space="preserve"
+          viewBox="0 0 24 24"
+          class="bi bi-facbook"
+          fill="currentColor"
+          height="24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="currentColor"
+            d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"
+          ></path>
+        </svg>
+      </a>
+      <div class="tooltip">Facebook</div>
+    </li>
+    <li class="icon-content">
+      <a
+        data-social="instagram"
+        aria-label="Instagram"
+        href="https://www.instagram.com/"
+      >
+        <div class="filled"></div>
+        <svg
+          xml:space="preserve"
+          viewBox="0 0 16 16"
+          class="bi bi-instagram"
+          fill="currentColor"
+          height="16"
+          width="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="currentColor"
+            d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
+          ></path>
+        </svg>
+      </a>
+      <div class="tooltip">Instagram</div>
+    </li>
+  </ul>
+
+            </div>						
+          </div>
+        </footer>	
+        <!-- End footer Area -->	
+
+
+        <script src="<?php echo AppURL; ?>js/vendor/jquery-2.2.4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="<?php echo AppURL; ?>js/vendor/bootstrap.min.js"></script>			
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+          <script src="<?php echo AppURL; ?>js/easing.min.js"></script>			
+        <script src="<?php echo AppURL; ?>js/hoverIntent.js"></script>
+        <script src="<?php echo AppURL; ?>js/superfish.min.js"></script>	
+        <script src="<?php echo AppURL; ?>js/jquery.ajaxchimp.min.js"></script>
+        <script src="<?php echo AppURL; ?>js/jquery.magnific-popup.min.js"></script>	
+          <script src="<?php echo AppURL; ?>js/jquery.tabs.min.js"></script>						
+        <script src="<?php echo AppURL; ?>js/jquery.nice-select.min.js"></script>	
+        <script src="<?php echo AppURL; ?>js/owl.carousel.min.js"></script>									
+        <script src="<?php echo AppURL; ?>js/mail-script.js"></script>	
+        <script src="<?php echo AppURL; ?>js/main.js"></script>
+              <script src="<?php echo AppURL; ?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery-ui.js"></script>
+    <script src="<?php echo AppURL; ?>js/popper.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/owl.carousel.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.stellar.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.countdown.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/bootstrap-datepicker.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.easing.1.3.js"></script>
+    <script src="<?php echo AppURL; ?>js/aos.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.fancybox.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.sticky.js"></script>
+    <script src="<?php echo AppURL; ?>js/jquery.mb.YTPlayer.min.js"></script>
+    <script src="<?php echo AppURL; ?>js/main_2.js"></script>	
+      </body>
+    </html>
