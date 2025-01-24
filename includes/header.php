4369356a787f82f58285b9e086965c7f9885c0bf
michaelxxf <miokonkwo023@gmail.com>
2025-01-24 04:54:51 +0000

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,156 @@
+<?php 
+  define("AppURL", "http://localhost:5500/")
+
+?>
+	<!DOCTYPE html>
+		<html lang="en" class="no-js">
+		<head>
+			<!-- Mobile Specific Meta -->
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<!-- Favicon-->
+			<link rel="shortcut icon" href="img/fav.png">
+			<!-- Author Meta -->
+			<meta name="author" content="colorlib">
+			<!-- Meta Description -->
+			<meta name="description" content="">
+			<!-- Meta Keyword -->
+			<meta name="keywords" content="">
+			<!-- meta character set -->
+			<meta charset="UTF-8">
+			<!-- Site Title -->
+			<title> IMT Education</title>
+
+			<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+				<!--
+				CSS
+				============================================= -->
+				<link rel="stylesheet" href="../css/linearicons.css">
+				<link rel="stylesheet" href="../css/font-awesome.min.css">
+				<link rel="stylesheet" href="../css/bootstrap.css">
+				<link rel="stylesheet" href="../css/magnific-popup.css">
+				<link rel="stylesheet" href="../css/nice-select.css">							
+				<link rel="stylesheet" href="../css/animate.min.css">
+				<link rel="stylesheet" href="../css/owl.carousel.css">			
+				<link rel="stylesheet" href="../css/jquery-ui.css">			
+				<link rel="stylesheet" href="../css/main.css">
+				<link rel="stylesheet" href="../css/style.css">
+				<link rel="stylesheet" href="../css/bootstrap.min.css">
+				<!-- <link rel="stylesheet" href="css/jquery-ui.css"> -->
+				<link rel="stylesheet" href="../css/owl.carousel.min.css">
+				<link rel="stylesheet" href="../css/owl.theme.default.min.css">
+				<link rel="stylesheet" href="../css/owl.theme.default.min.css">
+				<link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+				<link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+				<link rel="stylesheet" href="../fonts/flaticon/font/flaticon.css">
+				<link rel="stylesheet" href="../css/aos.css">
+				<link href="../css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+				<link rel="stylesheet" href="../css/style_2.css">
+			</head>
+			<body>
+				<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>
+			<header id="header" id="home">
+				<div class="header-top bg-white">
+					<div class="container">
+						<div class="row">
+							<div class="row-reverse col-sm-12 header-top-right no-padding flex-row-reverse ">
+								<a href="<?php echo AppURL; ?>index.html"><img class="logo" id="logo_img" src="<?php echo AppURL; ?>img/image.png" alt="" title="" style="height: 50px;"/></a>
+								<button class="btn text-primary gap-2 " style="border: 1.5px solid #ededed; background-color: white; border-radius: 0;">Alumni</button>
+								<button class="btn text-primary mx-2 " style="border: 1.5px solid #ededed; background-color: white; border-radius: 0;">Apply</button>
+								<button class="btn btn-primary text-white" style="border-radius: 0;">My IMT Portal</button>
+							</div>
+						</div>			  					
+					</div>
+				<!-- </div> -->
+				<div class="container main-menu" id="try">
+					<div class="row align-items-center justify-content-between d-flex flex-row-reverse">
+					<nav id="nav-menu-container">
+						<ul class="nav-menu ">
+						<li><a href="<?php echo AppURL; ?>index.html" class="">Home</a></li>
+						<li><a href="<?php echo AppURL; ?>about.html" class="">About IMT</a></li>
+						<li class="menu-has-children"><a href="<?php echo AppURL; ?>" class="">Media</a>
+							<ul class="">
+							<li ><a href="<?php echo AppURL; ?>blog-home.html" class="">News</a></li>
+							<li ><a href="<?php echo AppURL; ?>events.html" class="">Events</a></li>
+							<li><a href="<?php echo AppURL; ?>gallery.html" class="">Gallery</a></li>
+							</ul>
+						</li>	
+						<li class="menu-has-children"><a href="" class="">Academics</a>
+							<ul>
+								<li><a href="<?php echo AppURL; ?>course-details.html">Academic Calendar</a></li>		
+								<li><a href="<?php echo AppURL; ?>event-details.html">Academic Policies</a></li>
+								<li><a href="<?php echo AppURL; ?>event-details.html">Student Handbook</a></li>
+								<li><a href="<?php echo AppURL; ?>courses.html">Course Registeration</a></li>
+								<li><a href="<?php echo AppURL; ?>courses.html">Programmes Offered</a></li>
+								<li class="menu-has-children"><a href="<?php echo AppURL; ?>">Support Services</a>
+									<ul>
+									<li><a href="<?php echo AppURL; ?>#">Academic Advising</a></li>
+									<li><a href="<?php echo AppURL; ?>#">The library Divison</a></li>
+									</ul>
+								</li>					              
+								<li><a href="<?php echo AppURL; ?>courses.html">Certificates</a></li>
+								<li><a href="<?php echo AppURL; ?>courses.html">iTranscript Application</a></li> 		
+							</ul>
+						</li>					          					          		          
+						<li><a href="<?php echo AppURL; ?>contact.html" class="">Contact</a></li>
+						</ul>
+					</nav> <!--#nav-menu-container		    		 -->
+					</div>
+				</div>
+			</div>
+			</header>
+			<section>
+				<!-- From Uiverse.io by MijailVillegas --> 
+				<div class="card-chat">
+					<ul>
+					<li class="iso-pro">
+						<span></span>
+						<span></span>
+						<span></span>
+						<a href="">
+						<svg
+							viewBox="0 0 320 512"
+							xmlns="http://www.w3.org/2000/svg"
+							class="svg-chat"
+						>
+							<path
+							d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+							></path></svg></a>
+						<div class="text">Facebook</div>
+					</li>
+					<li class="iso-pro">
+						<span></span>
+						<span></span>
+						<span></span>
+						<a href="">
+						<svg
+							class="svg-chat"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 512 512"
+						>
+							<path
+							d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+							></path>
+						</svg>
+						</a>
+						<div class="text">Twitter</div>
+					</li>
+					<li class="iso-pro">
+						<span></span>
+						<span></span>
+						<span></span>
+						<a href="">
+						<svg
+							class="svg-chat"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 448 512"
+						>
+							<path
+							d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+							></path>
+						</svg>
+						</a>
+						<div class="text">Instagram</div>
+					</li>
+					</ul>
+				</div>
+			</section>  
